92a091a3e1091a61c7665fe0247dbf9e06fbc449
Nyasha-Ojha <146384561+Nyasha-Ojha@users.noreply.github.com>
2024-07-21 20:58:39 +0000

    datatypes completed

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all code as newer version
+
+
+// alert("Hello");  //Cant run this since we are usingg nodejs and not the browser
+
+// tc39.es --> original documentation of js
+
+/*datatypes 
+number
+bigInt
+null: representation of empty value
+undefined: not defined/initialized yet
+string: " "
+boolean: true/false
+symbol: unique
+*/
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
